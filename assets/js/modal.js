b5df3b150525e1f4b98d2e842e438c505ffd96e4
almns <126156174+almonsour13@users.noreply.github.com>
2023-09-15 06:16:25 -0700

    Add files via upload

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -36,7 +36,7 @@ function setupEventListeners(){
                         <path d="M480-80q-82 0-155-31.5t-127.5-86Q143-252 111.5-325T80-480q0-83 32.5-156t88-127Q256-817 330-848.5T488-880q80 0 151 27.5t124.5 76q53.5 48.5 85 115T880-518q0 115-70 176.5T640-280h-74q-9 0-12.5 5t-3.5 11q0 12 15 34.5t15 51.5q0 50-27.5 74T480-80Zm0-400Zm-220 40q26 0 43-17t17-43q0-26-17-43t-43-17q-26 0-43 17t-17 43q0 26 17 43t43 17Zm120-160q26 0 43-17t17-43q0-26-17-43t-43-17q-26 0-43 17t-17 43q0 26 17 43t43 17Zm200 0q26 0 43-17t17-43q0-26-17-43t-43-17q-26 0-43 17t-17 43q0 26 17 43t43 17Zm120 160q26 0 43-17t17-43q0-26-17-43t-43-17q-26 0-43 17t-17 43q0 26 17 43t43 17ZM480-160q9 0 14.5-5t5.5-13q0-14-15-33t-15-57q0-42 29-67t71-25h70q66 0 113-38.5T800-518q0-121-92.5-201.5T488-800q-136 0-232 93t-96 227q0 133 93.5 226.5T480-160Z"/>
                       </svg>
                         <div class="background">
-                            ${colorPalette()} <!-- Output of colorPalette function -->
+                            ${colorPalette()}
                             ${backgroundImage()} 
                         </div>
                     </a>
@@ -68,8 +68,8 @@ setupEventListeners()
 window.addEventListener("hashchange", setupEventListeners);
 function colorPalette() {
       const lightColors = [
-          '#F0E68C', 
-          '#98FB98', 
+          'white',
+          '#F0E68C',
           '#ADD8E6', 
           '#FFD700', 
           '#FFA07A',
@@ -77,7 +77,7 @@ function colorPalette() {
       ];
       let palette = ''; 
       lightColors.forEach(color => {
-          palette += `<li style="background-color: ${color};"></li>`;
+          palette += `<li class="color" style="background-color: ${color};" onclick="selectBackground(this)"></li>`;
       });
       return `<ul class="color-palette">${palette}</ul>`
 }    
@@ -92,31 +92,91 @@ function backgroundImage(){
       ];
       let background = '';
       backgroundImages.forEach(image => {
-          background += `<li class="backgroundImage" style="background-image: url(${image});"></li>`;
+          background += `<li class="backgroundImage" style="background-image: url(${image});" onclick="selectBackground(this)"></li>`;
       });
       return `<ul class="background-image">${background}</ul>`
 }
-  
+const footerButtons = document.querySelector('.footer-btn a');
+
+footerButtons.forEach(button => {
+    console.log(button)
+//   button.addEventListener('click', function (event) {
+//     event.preventDefault();
+//     alert("")
+//     footerButtons.forEach(otherButton => {
+//       otherButton.classList.remove('active');
+//     });
+//     button.classList.add('active');
+ // });
+});
+
 function reminder(element){
-    if (element.classList.contains("active")) {
-        element.classList.remove("active");
-    } else {
-        element.classList.add("active");
-    }
-  }
+    const buttons = document.querySelectorAll('.footer-btn a');
+    buttons.forEach(button => {
+      if (button !== element) {
+        button.classList.remove('active');
+      }
+    });
+    element.classList.toggle('active');
+}
 function backgroundColor(element){
-    if (element.classList.contains("active")) {
-        element.classList.remove("active");
-    } else {
-        element.classList.add("active");
+    const buttons = document.querySelectorAll('.footer-btn a');
+    buttons.forEach(button => {
+      if (button !== element) {
+        button.classList.remove('active');
+      }
+    });
+    element.classList.toggle('active');
+}
+function archive(element){
+    const buttons = document.querySelectorAll('.footer-btn a');
+    buttons.forEach(button => {
+      if (button !== element) {
+        button.classList.remove('active');
+      }
+    });
+    element.classList.toggle('active');
+}
+function deleteBtn(element){
+    const buttons = document.querySelectorAll('.footer-btn a');
+    buttons.forEach(button => {
+      if (button !== element) {
+        button.classList.remove('active');
+      }
+    });
+    element.classList.toggle('active');
+}
+function selectBackground(element) {
+    const parent = element.parentElement;
+  
+    // Find the items within the same parent
+    const items = parent.querySelectorAll('.color, .backgroundImage');
+    
+    items.forEach(item => {
+      if (item.classList.contains('active')) {
+        item.classList.remove('active');
+        console.log(item.classList);
+      }
+    });
+  
+    // Add 'active' class to the clicked element
+    element.classList.add('active');
+  
+    const computedStyle = window.getComputedStyle(element);
+    const backgroundColor = computedStyle.getPropertyValue('background-color');
+    const backgroundImage = computedStyle.getPropertyValue('background-image');
+    const background = document.querySelector(".modal-content");
+    background.style.backgroundColor = '';
+    background.style.backgroundImage = '';
+    background.style.background = 'none';
+  
+    if (element.classList.contains('color')) {
+      background.style.backgroundColor = backgroundColor;
+    } else if (element.classList.contains('backgroundImage')) {
+      background.style.backgroundImage = backgroundImage;
     }
 }
-function archive(){
-      alert()
-}
-function deleteBtn(){
-      alert()
-}
+  
 function closeModal(event) {
     event.preventDefault()
     const modalContainer = event.target.closest('.modal-container');
